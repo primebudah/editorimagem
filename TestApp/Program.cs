adb43f4afca25ed414981a434ba4132be7fade5d
primebudah <primebudah@gmail.com>
2026-07-27 11:47:57 +0900

    Remove unnecessary projects and files (WPF, Blazor, tests, installer)

--- TestApp/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-Console.WriteLine("Teste de .NET Runtime");
-Console.WriteLine("Se você ver esta mensagem, o .NET está funcionando!");
-Console.WriteLine("Pressione qualquer tecla para sair...");
-Console.ReadKey();
